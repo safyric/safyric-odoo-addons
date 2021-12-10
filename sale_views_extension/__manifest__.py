e3b429448151f55da489911bd77a3ce28a155906
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-10 11:11:19 +0800

    Create __manifest__.py

---
+++ sale_views_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Sale Views Extension',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Extend views to customize as per company',
+    'depends': [
+        'sale',
+    ],
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+}
