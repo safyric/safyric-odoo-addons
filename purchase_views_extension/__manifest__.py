a081069355942b59bff0f10da486db2a48f6d05b
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-10 21:14:43 +0800

    Update __manifest__.py

--- purchase_views_extension/__manifest__.py
+++ purchase_views_extension/__manifest__.py
@@ -1,1 +1,15 @@
-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Views Extension",
+    'summary': 'Extend views to customize as per company',
+    "author": "Safyric Co., Ltd.",
+    "version": "14.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
