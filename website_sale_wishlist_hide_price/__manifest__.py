da8fad8686aa200a0fda85de57a17b96fdd046c8
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-14 08:43:26 +0800

    Create __manifest__.py

---
+++ website_sale_wishlist_hide_price/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Sale Wishlist Hide Price',
+    'version': '12.0.1.0.0',
+    'category': 'Website',
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'license': 'AGPL-3',
+    'summary': 'Hide product prices on the shop (wishlist)',
+    'depends': [
+        'website_sale',
+        'website_sale_wishlist',
+        'website_sale_hide_price',
+    ],
+    'data': [
+        'views/website_sale_wishlist.xml'
+    ],
+    'installable': True,
+}
