3570955aa7f87e537e7589e42bd5b44211bb0c53
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-14 23:30:35 +0800

    Create __manifest__.py

---
+++ website_sale_ux/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale UX",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "license": "AGPL-3",
+    "summary": "Website Sale UI",
+    "depends": ["website_sale"],
+    "data": [
+      "views/website_sale_view.xml", 
+    ],
+    "installable": True,
+}
