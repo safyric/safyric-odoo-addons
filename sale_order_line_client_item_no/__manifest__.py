0a80c33c8370028ef7714bd63f661bcc0c3ee62f
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-15 22:31:25 +0800

    Create __manifest__.py

---
+++ sale_order_line_client_item_no/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Order Line Client Item No",
+    "version": "12.0.0.1",
+    "summary": "Adds client's item number to each sale order line.",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://www.safyric.com",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
