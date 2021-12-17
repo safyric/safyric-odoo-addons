0eba5246560a2180f8a6437b04b04d1eee5af277
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-17 17:20:27 +0800

    Update purchase_order.py

--- purchase_order_amount_in_words/models/purchase_order.py
+++ purchase_order_amount_in_words/models/purchase_order.py
@@ -1,51 +1,102 @@
 from odoo import api, fields, models
+from io import StringIO
+import math
 
 class PurchaseOrder(models.Model):
 
     _inherit = 'purchase.order'
     
-    @api.model
-    def Num2MoneyFormat(self, change_number):
-        format_word = ["分", "角", "元","拾", "百", "千", "万", "拾", "百", "千", "亿","拾", "百", "千", "万", "拾", "百", "千", "兆"]
 
-        format_num = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
+    _RMB_DIGITS = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' ]
+    _SECTION_CHARS = ['', '拾', '佰', '仟', '万' ]
 
-        if type(change_number) == float:
-            real_numbers = []
-            for i in range(len(format_word) - 3, -3, -1):
-                if change_number >= 10 ** i or i < 1:
-                    real_numbers.append(
-                        int(round(change_number/(10**i), 2) % 10))
+    def to_rmb_upper(price):
+        price = round(price, 2)
+        integer_part = int(price)
+        wanyi_part = integer_part // 1000000000000
+        yi_part = integer_part % 1000000000000 // 100000000
+        wan_part = integer_part % 100000000 // 10000
+        qian_part = integer_part % 10000
+        dec_part = int(price * 100 % 100)
 
-        elif isinstance(change_number, (int, long)):
-            real_numbers = [int(i) for i in str(change_number) + '00']
+        strio = StringIO()
 
-        zflag = 0  # 标记连续0次数，以删除万字，或适时插入零字
-        start = len(real_numbers) - 3
-        change_words = []
-        for i in range(start, -3, -1):  # 使i对应实际位数，负数为角分
-            if real_numbers[start-i] <> 0 or len(change_words) == 0:
-                if zflag:
-                    change_words.append(format_num[0])
-                    zflag = 0
-                change_words.append(format_num[real_numbers[start - i]])
-                change_words.append(format_word[i+2])
+        zero_count = 0
+        #处理万亿以上的部分
+        if integer_part >= 1000000000000 and wanyi_part > 0:
+            zero_count = _parse_integer(strio, wanyi_part, zero_count, True)
+            strio.write('万')
 
-            elif 0 == i or (0 == i % 4 and zflag < 3):  # 控制 万/元
-                change_words.append(format_word[i+2])
-                zflag = 0
+        #处理亿到千亿的部分
+        if integer_part >= 100000000 and yi_part > 0:
+            is_first_section = integer_part >= 100000000 and integer_part < 1000000000000 
+            zero_count = _parse_integer(strio, yi_part, zero_count, is_first_section)
+            strio.write('亿')
+
+        #处理万的部分
+        if integer_part >= 10000 and wan_part > 0:
+            is_first_section = integer_part >= 1000 and integer_part < 10000000 
+            zero_count = _parse_integer(strio, wan_part, zero_count, is_first_section)
+            strio.write('万')
+
+        #处理千及以后的部分
+        if qian_part > 0:
+            is_first_section = integer_part < 1000
+            zero_count = _parse_integer(strio, qian_part, zero_count, is_first_section)
+        else:
+            zero_count += 1
+        if integer_part > 0:
+            strio.write('元')
+
+        #处理小数
+        if dec_part > 0: 
+            _parse_decimal(strio, integer_part, dec_part, zero_count)
+        elif dec_part == 0 and integer_part > 0:
+            strio.write('整')
+        else:
+            strio.write('零元整')
+
+        return strio.getvalue()
+
+    def _parse_integer(strio, value, zero_count = 0, is_first_section = False):
+        assert value > 0 and value <= 9999
+        ndigits = int(math.floor(math.log10(value))) + 1
+        if value < 1000 and not is_first_section:
+            zero_count += 1
+        for i in range(0, ndigits):
+            factor = int(pow(10, ndigits - 1 - i))
+            digit = int(value / factor)
+            if digit != 0:
+                if zero_count > 0:
+                    strio.write('零')
+                strio.write(_RMB_DIGITS[digit])
+                strio.write(_SECTION_CHARS[ndigits - i - 1])
+                zero_count = 0
             else:
-                zflag += 1
+                zero_count += 1
+            value -= value // factor * factor
+        return zero_count
 
-        if change_words[-1] not in (format_word[0], format_word[1]):
-            # - 最后两位非"角,分"则补"整"
-            change_words.append("整")
-
-        return ''.join(change_words)
+    def _parse_decimal(strio, integer_part, value, zero_count):
+        assert value > 0 and value <= 99
+        jiao = value // 10
+        fen = value % 10
+        if zero_count > 0 and (jiao > 0 or fen > 0) and integer_part > 0:
+            strio.write('零')
+        if jiao > 0:
+            strio.write(_RMB_DIGITS[jiao])
+            strio.write('角')
+        if zero_count == 0 and jiao == 0 and fen > 0 and integer_part > 0:
+            strio.write('零')
+        if fen > 0:
+            strio.write(_RMB_DIGITS[fen])
+            strio.write('分')
+        else:
+            strio.write('整')
 
         @api.multi
         def _compute_amount_in_words(self):
             for rec in self:
-                rec.amount_words = str(rec.Num2MoneyFormat(rec.amount_total))
+                rec.amount_words = str(rec.to_rmb_upper(rec.amount_total))
 
     amount_words = fields.Char(string="金额大写", compute='_compute_amount_in_words')
