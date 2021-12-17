e7433f8bf4084a1856a5e1e9ed47f239bca14f0c
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-17 11:45:11 +0800

    Update __manifest__.py

--- purchase_views_extension/__manifest__.py
+++ purchase_views_extension/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "Purchase Views Extension",
     'summary': 'Extend views to customize as per company',
     "author": "Safyric Co., Ltd.",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Purchase Management",
     "website": "https://github.com/safyric/safyric-odoo-addons",
     "depends": ["purchase"],
