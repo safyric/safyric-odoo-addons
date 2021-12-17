8fd3cc26a36f98d0a02acd77c14dbe4cc422e7da
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-17 23:47:11 +0800

    Update purchase.py

--- purchase_order_digitized_signature/models/purchase.py
+++ purchase_order_digitized_signature/models/purchase.py
@@ -18,11 +18,11 @@ class PurchaseOrder(models.Model):
     def create(self, values):
         purchase = super(PurchaseOrder, self).create(values)
         if purchase.seller_signature:
-            values = {'seller_signature': purchase.seller_signature, 'buyer_signature': purchase.buyer_signature}
-            purchase._track_signature(values, 'seller_signature', 'buyer_signature')
+            values = {'seller_signature': purchase.seller_signature}
+            purchase._track_signature(values, 'seller_signature')
         return purchase
 
     @api.multi
     def write(self, values):
-        self._track_signature(values, 'seller_signature', 'buyer_signature')
+        self._track_signature(values, 'seller_signature')
         return super(PurchaseOrder, self).write(values)
