4ced7d820445b774320333b2d54322821e619b1b
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-17 12:48:03 +0800

    Create purchase_order.py

---
+++ purchase_order_amount_in_words/models/purchase_order.py
@@ -0,0 +1,10 @@
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def _compute_amount_in_words(self):
+        for rec in self:
+            rec.amount_words = str(rec.currency_id.amount_to_text(rec.amount_total)) + ' only'
+
+    amount_words = fields.Char(string="Amount In Words:", compute='_compute_amount_in_word')
