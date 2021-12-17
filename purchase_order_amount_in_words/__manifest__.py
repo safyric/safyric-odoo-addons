ea178fcc2b9bc679bb5ba498c4d3887261760715
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-17 12:49:54 +0800

    Create __manifest__.py

---
+++ purchase_order_amount_in_words/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Amount in Words",
+    'summary': 'Show amount total in words for purchase order',
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.1",
+    "category": "Purchase Management",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
