0752e58ddf64954c6fe00b6d68fe5ea3b76ae82c
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 13:42:22 +0800

    Create __manifest__.py

---
+++ sale_view_extension/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Views Extension",
+    'summary': 'Extend views to customize as per company',
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.1",
+    "category": "Sales",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+      "sale",
+      "sale_management",
+    ],
+    "data": [
+        "views/sale_report_layout.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
