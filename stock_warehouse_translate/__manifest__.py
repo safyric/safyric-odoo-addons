35fbb8f020049ee10ccc2154967cfe820265e3ce
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 19:19:22 +0800

    Create __manifest__.py

---
+++ stock_warehouse_translate/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Warehouse Name Translate',
+    'version': '12.0.1.0.0',
+    'category': 'Inventory',
+    'license': 'AGPL-3',
+    'summary': 'Enable to translate the warehouse names',
+    'depends': [
+        'stock',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'installable': True,
+}
