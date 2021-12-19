351e43def35ceb3fde249ece01fcf91f3bbccee8
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 17:35:25 +0800

    Create __manifest__.py

---
+++ sale_order_digitized_signature_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Digitized Signature Extended",
+    "summary": "Capture customer signature on the purchase order",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "sale_order_digitized_signature",
+    ],
+    "data": [
+        "views/report_saleorder.xml",
+    ],
+    "installable": True,
+}
