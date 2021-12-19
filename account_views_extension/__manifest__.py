5d6c3793d233e4fdca6f76898f1a79ee79da4808
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 20:56:34 +0800

    Update __manifest__.py

--- account_views_extension/__manifest__.py
+++ account_views_extension/__manifest__.py
@@ -1,1 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
+{
+    'name': 'Invoicing Views Extension',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Extend views and reports for account module',
+    'depends': [
+        'account',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/account_invoice_report.xml',
+    ],
+    'installable': True,
+}
