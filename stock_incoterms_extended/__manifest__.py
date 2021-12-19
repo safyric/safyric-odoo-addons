316ea421a2ac571f870f28dd6f4b567536f06a4b
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 16:52:54 +0800

    Create __manifest__.py

---
+++ stock_incoterms_extended/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Incoterms Extended",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+
+    "category": "Stock",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "license": "AGPL-3",
+    'data': [
+        'security/ir.model.access.csv',
+        'data/stock_delivery_place.xml',
+        'views/sale_order_view.xml',
+        'views/stock_delivery_view.xml',
+        'views/report_sale_order.xml',
+    ],
+    "depends": [
+        "stock",
+        "sale",
+    ],
+    "installable": True,
+}
