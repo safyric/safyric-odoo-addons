6ff83339b94c2649016731f976b0f294568e0db2
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 20:12:47 +0800

    Create __manifest__.py

---
+++ base_email_address/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Email Address for Reports',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Email address for use in reports and other documents',
+    'depends': [
+        'base',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/res_config.xml',
+    ],
+    'installable': True,
+}
