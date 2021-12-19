6d118b5d105434790fb5176c0166fcccd81e6b1d
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-19 16:24:00 +0800

    Create __manifest__.py

---
+++ sale_order_general_lead_time/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Order General Lead Time",
+    "version": "12.0.0.1",
+    "summary": "General lead time per sale order",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
