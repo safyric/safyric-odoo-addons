f02c21fb5a79f4f9dc283fbc08eca2a2ef8c844d
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 14:46:45 +0800

    Update purchase_order.py

--- purchase_order_revision/models/purchase_order.py
+++ purchase_order_revision/models/purchase_order.py
@@ -18,38 +18,3 @@ class PurchaseOrder(models.Model):
             vals['unrevisioned_name'] = vals['name']
         return super(PurchaseOrder, self).create(vals)
     
-    @api.multi
-    def action_revision(self):
-        self.ensure_one()
-        view_ref = self.env['ir.model.data'].get_object_reference('purchase', 'purchase_order_form')
-        view_id = view_ref and view_ref[1] or False,
-        self.with_context(purchase_revision_history=True).copy()
-        self.write({'state': 'draft'})
-        self.order_line.write({'state': 'draft'})
-        self.mapped('order_line').write(
-            {'purchase_line_id': False})
-        return {
-            'type': 'ir.actions.act_window',
-            'name': _('Purchase Order'),
-            'res_model': 'purchase.order',
-            'res_id': self.id,
-            'view_type': 'form',
-            'view_mode': 'form',
-            'view_id': view_id,
-            'target': 'current',
-            'nodestroy': True,
-        }
-        
-    @api.returns('self', lambda value: value.id)
-    @api.multi
-    def copy(self, defaults=None):
-        if not defaults:
-            defaults = {}
-        if not self.unrevisioned_name:
-            self.unrevisioned_name = self.name
-        if self.env.context.get('purchase_revision_history'):
-            prev_name = self.name
-            revno = self.revision_number
-            self.write({'revision_number': revno + 1,'name': '%s-%02d' % (self.unrevisioned_name,revno + 1)})
-            defaults.update({'name': prev_name,'revision_number': revno,'active': False,'state': 'cancel','current_revision_id': self.id,'unrevisioned_name': self.unrevisioned_name,})
-        return super(PurchaseOrder, self).copy(defaults)
