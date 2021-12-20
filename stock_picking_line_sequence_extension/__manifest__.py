fc75b7301624e5e7ae7fee91427508c380dee36a
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 22:44:07 +0800

    Create __manifest__.py

---
+++ stock_picking_line_sequence_extension/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock picking lines sequence extension',
+    'summary': 'Manages the order of stock moves by displaying its sequence',
+    'version': '12.0.1.0.0',
+    'category': 'Warehouse Management',
+    'author': "Safyric Co., Ld.",
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': ['stock_picking_line_sequence'],
+    'data': [
+        'report/report_deliveryslip.xml',
+    ],
+    'installable': True,
+    'license': "AGPL-3",
+}
