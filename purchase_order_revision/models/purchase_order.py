31f636778150b11c3e1b14ad2b2a068914af40c9
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 12:46:07 +0800

    Update purchase_order.py

--- purchase_order_revision/models/purchase_order.py
+++ purchase_order_revision/models/purchase_order.py
@@ -1,121 +1,55 @@
-# Based on OCA module sale_order_revision.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from odoo import api, fields, models, _
-
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    @api.depends('current_revision_id', 'old_revision_ids')
-    def _compute_has_old_revisions(self):
-        for purchase_order in self:
-            if purchase_order.old_revision_ids:
-                purchase_order.has_old_revisions = True
-
-    current_revision_id = fields.Many2one(
-        comodel_name='purchase.order',
-        string='Current revision',
-        readonly=True,
-        copy=True
-    )
-    old_revision_ids = fields.One2many(
-        comodel_name='purchase.order',
-        inverse_name='current_revision_id',
-        string='Old revisions',
-        readonly=True,
-        context={'active_test': False}
-    )
-    revision_number = fields.Integer(
-        string='Revision',
-        copy=False,
-        default=0
-    )
-    unrevisioned_name = fields.Char(
-        string='Original Order Reference',
-        copy=True,
-        readonly=True
-    )
-    active = fields.Boolean(
-        default=True
-    )
-    has_old_revisions = fields.Boolean(
-        compute='_compute_has_old_revisions')
-
-    _sql_constraints = [
-        ('revision_unique',
-         'unique(unrevisioned_name, revision_number, company_id)',
-         'Order Reference and revision must be unique per Company.'),
-    ]
-
-    @api.multi
-    @api.returns('self', lambda value: value.id)
-    def copy(self, default=None):
-        if default is None:
-            default = {}
-        if default.get('name', '/') == '/':
-            seq = self.env['ir.sequence']
-            default['name'] = seq.next_by_code('purchase.order') or '/'
-            default['revision_number'] = 0
-            default['unrevisioned_name'] = default['name']
-        return super(PurchaseOrder, self).create(default=default)
-
-    def copy_revision_with_context(self):
-        default_data = self.default_get([])
-        new_rev_number = self.revision_number + 1
-        default_data.update({
-            'revision_number': new_rev_number,
-            'unrevisioned_name': self.unrevisioned_name,
-            'name': '%s-%02d' % (self.unrevisioned_name, new_rev_number),
-            'old_revision_ids': [(4, self.id, False)],
-        })
-        new_revision = self.copy(default_data)
-        self.old_revision_ids.write({
-            'current_revision_id': new_revision.id,
-        })
-        self.write({'active': False,
-                    'state': 'cancel',
-                    'current_revision_id': new_revision.id,
-                    })
-
-        return new_revision
+    current_revision_id = fields.Many2one('purchase.order','Current revision',readonly=True,copy=True)
+    old_revision_ids = fields.One2many('purchase.order','current_revision_id','Old revisions',readonly=True,context={'active_test': False})
+    revision_number = fields.Integer('Revision',copy=False)
+    unrevisioned_name = fields.Char('Order Reference',copy=False,readonly=True)
+    active = fields.Boolean('Active',default=True,copy=True)
 
     @api.model
-    def create(self, values):
-        if 'unrevisioned_name' not in values:
-            if values.get('name', '/') == '/':
+    def create(self, vals):
+        if 'unrevisioned_name' not in vals:
+            if vals.get('name', 'New') == 'New':
                 seq = self.env['ir.sequence']
-                values['name'] = seq.next_by_code('purchase.order') or '/'
-            values['unrevisioned_name'] = values['name']
-        return super(PurchaseOrder, self).create(values)
+                vals['name'] = seq.next_by_code('purchase.order') or '/'
+            vals['unrevisioned_name'] = vals['name']
+        return super(PurchaseOrder, self).create(vals)
 
     @api.multi
-    def create_revision(self):
-
-        revision_ids = []
-        # Looping over sale order records
-        for purchase_order_rec in self:
-            # Calling  Copy method
-            copied_purchase_rec = purchase_order_rec.copy_revision_with_context()
-
-            msg = _('New revision created: %s') % copied_purchase_rec.name
-            copied_purchase_rec.message_post(body=msg)
-            purchase_order_rec.message_post(body=msg)
-
-            revision_ids.append(copied_purchase_rec.id)
-
-        action = {
+    def action_revision(self):
+        self.ensure_one()
+        view_ref = self.env['ir.model.data'].get_object_reference('purchase', 'purchase_order_form')
+        view_id = view_ref and view_ref[1] or False,
+        self.with_context(purchase_revision_history=True).copy()
+        self.write({'state': 'draft'})
+        self.order_line.write({'state': 'draft'})
+        self.mapped('order_line').write(
+            {'purchase_line_id': False})
+        return {
             'type': 'ir.actions.act_window',
-            'name': _('New Revisions'),
+            'name': _('Purchase Order'),
             'res_model': 'purchase.order',
-            'domain': "[('id', 'in', %s)]" % revision_ids,
-            'auto_search': True,
-            'views': [
-                (self.env.ref('purchase.purchase_order_tree').id, 'tree'),
-                (self.env.ref('purchase.purchase_order_form').id, 'form')],
+            'res_id': self.id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'view_id': view_id,
             'target': 'current',
             'nodestroy': True,
         }
 
-        # Returning the new sale order view with new record.
-        return action
+    @api.returns('self', lambda value: value.id)
+    @api.multi
+    def copy(self, defaults=None):
+        if not defaults:
+            defaults = {}
+        if not self.unrevisioned_name:
+            self.unrevisioned_name = self.name
+        if self.env.context.get('purchase_revision_history'):
+            prev_name = self.name
+            revno = self.revision_number
+            self.write({'revision_number': revno + 1,'name': '%s-%02d' % (self.unrevisioned_name,revno + 1)})
+            defaults.update({'name': prev_name,'revision_number': revno,'active': False,'state': 'cancel','current_revision_id': self.id,'unrevisioned_name': self.unrevisioned_name,})
+        return super(PurchaseOrder, self).copy(defaults)
