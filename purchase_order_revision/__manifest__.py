e1d50e42811a25a0307d2a218a3d97818388eb58
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 11:52:36 +0800

    Create __manifest__.py

---
+++ purchase_order_revision/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Revision",
+    'summary': 'Keep track of revised quotations',
+    "author": "Safyric Co., Ltd.",
+    "version": "14.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
