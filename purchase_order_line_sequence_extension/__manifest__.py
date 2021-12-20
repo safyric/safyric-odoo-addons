c9044c4253bfdcbeebe30c1495d550415f5f0f3c
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 22:28:38 +0800

    Create __manifest__.py

---
+++ purchase_order_line_sequence_extension/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Purchase Order Line Sequence Extension',
+    'summary': 'Adds sequence to PO lines and propagates it to'
+               'Invoice lines and Stock Moves',
+    'version': '12.0.1.0.0',
+    'category': 'Purchase Management',
+    'author': "Safyric Co., Ltd.",
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': [
+        'purchase_order_line_sequence',
+    ],
+    'data': [
+      'views/report_purchaseorder.xml',
+      'views/report_purchasequotation.xml'
+    ],
+    'installable': True,
+    'license': "AGPL-3",
+}
