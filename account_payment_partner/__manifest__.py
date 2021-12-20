07bbc450b4a16c89ed64ec362fad9355471b0156
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 16:31:38 +0800

    Create __manifest__.py

---
+++ account_payment_partner/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Payment Partner Extended',
+    'version': '12.0.1.0.0',
+    'category': 'Banking addons',
+    'license': 'AGPL-3',
+    'summary': 'Adds payment mode on partners and invoices',
+    'author': "Safyric Co., Ltd.",
+    'website': 'https://github.com/OCA/bank-payment',
+    'depends': ['account_payment_partner'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+}
