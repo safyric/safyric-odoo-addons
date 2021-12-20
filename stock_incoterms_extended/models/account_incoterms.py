a3b8860f5b570e64f0d8cc9ef68fa53c2ba78bb4
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 23:30:03 +0800

    Create account_incoterms.py

---
+++ stock_incoterms_extended/models/account_incoterms.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+class AccountIncoterms(models.Model):
+    _inherit = 'account.incoterms'
+
+    def name_get(self):
+      result = []
+      for rec in self:
+          result.append((rec.id, '%s - %s' % (rec.code,rec.name)))
+
+      return result
