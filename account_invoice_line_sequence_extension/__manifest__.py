73b37f4e6f5abbaaf8e296c0d07fea0fce1653a7
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 22:56:13 +0800

    Create __manifest__.py

---
+++ account_invoice_line_sequence_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Invoice Line Sequence Extension',
+    'version': '12.0.1.0.0',
+    "summary": "Adds sequence field on invoice lines to manage its order.",
+    'category': 'Accounting',
+    'author': "Safyric Co., Ltd.",
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': ['views/report_invoice.xml'],
+    'depends': ['account_invoice_line_sequence'],
+    'license': 'AGPL-3',
+    'installable': True,
+}
