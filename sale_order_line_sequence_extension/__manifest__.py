0097d6edbbcdd2022ac9ecc35da08ad5ee1e12c3
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 22:11:08 +0800

    Create __manifest__.py

---
+++ sale_order_line_sequence_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Line Sequence Extension",
+    "summary": "Propagates SO line sequence to invoices and stock picking.",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "category": "Sales",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "license": "AGPL-3",
+    'data': [
+        'views/sale_view.xml',
+        'views/report_saleorder.xml'
+    ],
+    "depends": [
+        "sale_order_line_sequence",
+    ],
+    "installable": True,
+}
