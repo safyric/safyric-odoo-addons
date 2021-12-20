46d1018ee614fa47bbe11d801804f42b2a95cb56
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-20 16:25:42 +0800

    Create __manifest__.py

---
+++ account_payment_mode_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Payment Mode Extended',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Safyric Co., Ltd.",
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'category': 'Banking addons',
+    'depends': ['account_payment_mode'],
+    'data': [
+        'views/account_payment_mode.xml',
+    ],
+    'installable': True,
+}
