d74620b9d0c288575a351e7c00865a548eb407b0
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-21 12:42:26 +0800

    Create __manifest__.py

---
+++ sale_order_optional_products/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Sale Order Optional Products',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Extend views to customize as per company',
+    'depends': [
+        'sale_management',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/sale_report_layout.xml',
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+}
