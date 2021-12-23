775f23f950dccaeffacfc852d19bbaa5ee27f85e
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-23 21:09:22 +0800

    Update sale_order.py

--- sale_order_line_sequence_extension/models/sale_order.py
+++ sale_order_line_sequence_extension/models/sale_order.py
@@ -26,3 +26,12 @@ class SaleOrderLine(models.Model):
                 'item': self.item,
             })
         return res
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_id, name, origin, values, group_id):
+        res = super(StockRule, self)._get_stock_move_values(product_id, product_qty, product_uom, location_id,
+                                                           name, origin, values, group_id)
+        res['item'] = values.get('item', False)
+        return res
