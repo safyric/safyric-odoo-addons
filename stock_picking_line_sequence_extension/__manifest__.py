6fa6917a3f421c99eb2dc6fc3fbfc593027bc9b5
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-23 15:43:58 +0800

    Update __manifest__.py

--- stock_picking_line_sequence_extension/__manifest__.py
+++ stock_picking_line_sequence_extension/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Manages the order of stock moves by displaying its sequence',
     'version': '12.0.1.0.0',
     'category': 'Warehouse Management',
-    'author': "Safyric Co., Ld.",
+    'author': "Safyric Co., Ltd.",
     'website': 'https://github.com/safyric/safyric-odoo-addons',
     'depends': [
         'stock_picking_line_sequence',
