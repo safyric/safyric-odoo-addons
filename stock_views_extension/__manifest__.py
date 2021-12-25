433957824e89a0c8ad660e17a57c65713aa2bfa9
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-25 16:47:13 +0800

    Create __manifest__.py

---
+++ stock_views_extension/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Inventory Views Extension",
+    'summary': 'Extend views to customize as per company',
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+        "purchase",
+        "purchase_stock",
+    ],
+    "data": [
+        "views/stock_report_layout.xml",
+        "views/report_deliveryslip.xml",
+        "views/report_picking.xml",
+        "views/stock_picking_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
