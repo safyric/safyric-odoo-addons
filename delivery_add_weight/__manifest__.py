13cb8264eb2e9884829ba1a2f677d2393f300da4
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-28 12:58:43 +0800

    Create __manifest__.py

---
+++ delivery_add_weight/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Delivery Add Weight",
+    'summary': "Add weight to stock move line",
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+        "delivery",
+    ],
+    "data": [
+        "views/delivery_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
