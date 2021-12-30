3bdc282334dc1e1c2718601dfebffbb8294b2ed0
jchanme <39323414+jchanme@users.noreply.github.com>
2021-12-30 21:38:27 +0800

    Create __manifest__.py

---
+++ stock_picking_show_serialnumber/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Stock Picking Show Serial Number",
+    'summary': "Give option to show serial number/lot number on picking/delivery slip.",
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_report_layout.xml",
+        "views/stock_picking_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
