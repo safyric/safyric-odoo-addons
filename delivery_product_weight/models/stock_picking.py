803e00f6e7207ad4cd5f343caa355584b710b18d
jchanme <39323414+jchanme@users.noreply.github.com>
2022-01-06 08:58:44 +0800

    Update stock_picking.py

--- delivery_product_weight/models/stock_picking.py
+++ delivery_product_weight/models/stock_picking.py
@@ -15,23 +15,3 @@ class StockPicking(models.Model):
             else:
                 weight += move_line.product_uom_id._compute_quantity(move_line.qty_done, move_line.product_id.uom_id) * move_line.product_id.weight
         self.weight_bulk = weight
-
-
-class StockQuantPackage(models.Model):
-    _inherit = "stock.quant.package"
-
-    @api.one
-    @api.depends('quant_ids')
-    def _compute_weight(self):
-        weight = 0.0
-        if self.env.context.get('picking_id'):
-            current_picking_move_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id), ('picking_id', '=', self.env.context['picking_id'])])
-            for ml in current_picking_move_line_ids:
-                if ml.product_weight > 0:
-                    weight += ml.product_uom_id._compute_quantity(ml.qty_done,ml.product_id.uom_id) * ml.product_weight
-                else:
-                    weight += ml.product_uom_id._compute_quantity(ml.qty_done,ml.product_id.uom_id) * ml.product_id.weight
-        else:
-            for quant in self.quant_ids:
-                weight += quant.quantity * quant.product_id.weight
-        self.weight = weight
