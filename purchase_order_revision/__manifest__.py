6322783d1dd50c1398950736be0735945d15cb43
jchanme <39323414+jchanme@users.noreply.github.com>
2022-02-18 12:00:20 +0800

    Update __manifest__.py

--- purchase_order_revision/__manifest__.py
+++ purchase_order_revision/__manifest__.py
@@ -3,7 +3,7 @@
     "name": "Purchase Order Revision",
     'summary': 'Keep track of revised quotations',
     "author": "Safyric Co., Ltd.",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Purchase Management",
     "website": "https://github.com/safyric/safyric-odoo-addons",
     "depends": [
