52d97267ddeda574df1940caff8c4d4e7529a9e2
jchanme <39323414+jchanme@users.noreply.github.com>
2022-02-18 11:19:35 +0800

    Create __manifest__.py

---
+++ purchase_order_so_line/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Dsiplay SO Line",
+    'summary': 'Display Sale Order Line',
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
