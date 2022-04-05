88d53e5f46e641053e6768c9f415f3568c57a636
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-05 13:55:50 +0800

    Create __manifest__.py

---
+++ sale_crm_views_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Sale CRM Views Extension',
+    'version': '12.0.1.0.1',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Extend views to customize as per company',
+    'depends': [
+        'sale_crm',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/crm_lead_views.xml',
+    ],
+    'installable': True,
+}
