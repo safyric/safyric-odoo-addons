add6cd2264d1d78ca11a80aa69cc3a5aedcf2989
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-09 13:46:17 +0800

    Update __manifest__.py

--- product_price_extra_extension/__manifest__.py
+++ product_price_extra_extension/__manifest__.py
@@ -1,1 +1,16 @@
-
+{
+    'name': 'Product Attribute Extra Price',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Extend product attribute extra price',
+    'depends': [
+        'product',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/product_attribute.xml',
+    ],
+    'installable': True,
+}
