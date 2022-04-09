4219080f8386ac6b8564162cb0c3c34d1e455600
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-09 22:29:34 +0800

    Update product_attribute.py

--- product_price_extra_extension/models/product_attribute.py
+++ product_price_extra_extension/models/product_attribute.py
@@ -3,4 +3,7 @@ from odoo import api, fields, models
 class ProductTemplateAttributeValueInherit(models.Model):
     _inherit = "product.template.attribute.value"
 
-    price_extra_pct = fields.Boolean('Price Extra %')
+    price_extra_pct = fields.Float(
+        string='Attribute Price Extra %',
+        default=0.0,
+        digits=dp.get_precision('Product Price'))
