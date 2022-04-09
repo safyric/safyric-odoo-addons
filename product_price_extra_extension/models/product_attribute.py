aa9cc76334cddbefc7c9a3c26f1e5e4a6a3639cc
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-09 15:33:51 +0800

    Update product_attribute.py

--- product_price_extra_extension/models/product_attribute.py
+++ product_price_extra_extension/models/product_attribute.py
@@ -3,4 +3,4 @@ from odoo import api, fields
 class ProductTemplateAttributeValueInherit(models.Model):
     _inherit = "product.template.attribute.value"
 
-    price_extra_pct = fields.Boolean('is pct?')
+    price_extra_pct = fields.Boolean('Is price percentage?')
