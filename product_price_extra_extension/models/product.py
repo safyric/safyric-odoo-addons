a9f679cea654f3ae0ce54ec44acff5fe650695a3
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-09 16:20:48 +0800

    Update product.py

--- product_price_extra_extension/models/product.py
+++ product_price_extra_extension/models/product.py
@@ -6,10 +6,10 @@ class ProductPriceExtra(models.Model):
     def _compute_product_price_extra(self):
         for product in self:
             price_extra = 0.0
-            for attribute_id in product.mapped('product_tmpl_attribute_value_ids'):
+            for attribute_id in product.mapped('product_template_attribute_value_ids'):
                 if attribute_id.price_extra_pct:
-                    price_extra += product.product_tmpl_id.list_price * attribute_id.price_extra
+                    price_extra += product.standard_price * attribute_id.price_extra
                 else:
-                    price_extra +=attribute_id.price_extra
+                    price_extra += attribute_id.price_extra
 
             product.price_extra = sum(price_extra)
