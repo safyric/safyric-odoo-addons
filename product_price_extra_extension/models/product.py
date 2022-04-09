8332d1a65eb702cbf107d6a8dd497cbdde557799
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-09 22:28:36 +0800

    Update product.py

--- product_price_extra_extension/models/product.py
+++ product_price_extra_extension/models/product.py
@@ -3,13 +3,8 @@ from odoo import api, fields, models
 class ProductPriceExtra(models.Model):
     _inherit = 'product.product'
 
+    @api.depends('product_template_attribute_value_ids.price_extra', 'product_template_attribute_value_ids.price_extra_pct')
     def _compute_product_price_extra(self):
         for product in self:
-            price_extra = 0.0
-            for attribute_id in product.mapped('product_template_attribute_value_ids'):
-                if attribute_id.price_extra_pct:
-                    price_extra += product.price * attribute_id.price_extra / 100
-                else:
-                    price_extra += attribute_id.price_extra
-
-            product.price_extra = price_extra
+            product.price_extra = sum(product.mapped('product_template_attribute_value_ids.price_extra')) \
+            + sum(product.mapped('product_template_attribute_value_ids.price_extra_pct')) * product.price
