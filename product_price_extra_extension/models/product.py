42d00805db13a19d92b6c795c2b0b682f0d2ded5
jchanme <39323414+jchanme@users.noreply.github.com>
2022-04-16 16:03:12 +0800

    Update product.py

--- product_price_extra_extension/models/product.py
+++ product_price_extra_extension/models/product.py
@@ -14,17 +14,38 @@ class ProductPriceExtra(models.Model):
         for product in self:
             product.price_extra_pct = sum(product.mapped('product_template_attribute_value_ids.price_extra_pct'))
 
-    @api.depends('list_price', 'price_extra', 'price_extra_pct')
-    def _compute_product_lst_price(self):
-        to_uom = None
-        if 'uom' in self._context:
-            to_uom = self.env['uom.uom'].browse([self._context['uom']])
+    @api.multi
+    def price_compute(self, price_type, uom=False, currency=False, company=False):
+        # TDE FIXME: delegate to template or not ? fields are reencoded here ...
+        # compatibility about context keys used a bit everywhere in the code
+        if not uom and self._context.get('uom'):
+            uom = self.env['uom.uom'].browse(self._context['uom'])
+        if not currency and self._context.get('currency'):
+            currency = self.env['res.currency'].browse(self._context['currency'])
 
-        for product in self:
-            if to_uom:
-                list_price = product.uom_id._compute_price(product.list_price, to_uom)
-            else:
-                list_price = product.list_price
-            product.lst_price = (list_price + product.price_extra) * (1 + product.price_extra_pct / 100)
+        products = self
+        if price_type == 'standard_price':
+            # standard_price field can only be seen by users in base.group_user
+            # Thus, in order to compute the sale price from the cost for users not in this group
+            # We fetch the standard price as the superuser
+            products = self.with_context(force_company=company and company.id or self._context.get('force_company', self.env.user.company_id.id)).sudo()
 
-        return super(ProductProductExtra,self)._compute_product_lst_price()
+        prices = dict.fromkeys(self.ids, 0.0)
+        for product in products:
+            prices[product.id] = product[price_type] or 0.0
+            if price_type == 'list_price':
+                for line in product.product_tmpl_id.attribute_line_ids:
+                    for value in product.attribute_value_ids.filtered(lambda r: r.attribute_id == line.attribute_id):
+                        for price in value.price_ids.filtered(lambda r: r.product_tmpl_id == product.product_tmpl_id):
+                            prices[product.id] = (prices[product.id] + price.price_extra) * (1 + price.price_extra_pct / 100)
+
+            if uom:
+                prices[product.id] = product.uom_id._compute_price(prices[product.id], uom)
+
+            # Convert from current user company currency to asked one
+            # This is right cause a field cannot be in more than one currency
+            if currency:
+                prices[product.id] = product.currency_id._convert(
+                    prices[product.id], currency, product.company_id, fields.Date.today())
+
+        return prices
