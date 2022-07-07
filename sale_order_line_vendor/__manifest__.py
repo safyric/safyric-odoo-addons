ae2d8ad51f83fc60ce0ad2bad8ffb32d21baa693
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-07 20:44:36 +0800

    Create __manifest__.py

---
+++ sale_order_line_vendor/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Order Line Vendor",
+    "version": "12.0.0.1",
+    "summary": "Adds vendor selection to each sale order line.",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://www.safyric.com",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_line.xml",
+    ],
+    "installable": True,
+}
