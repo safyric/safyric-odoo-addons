655902518197015f7310485e153c569ae4ad338d
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-07 21:01:29 +0800

    Update sale_order_line.py

--- sale_order_line_vendor/models/sale_order_line.py
+++ sale_order_line_vendor/models/sale_order_line.py
@@ -1,1 +1,8 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo import api, fields, models
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    
+    product_vendor = fields.Many2one('product.supplierinfo', string='Vendor', default=False)
