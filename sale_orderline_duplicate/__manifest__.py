cae6f6ac43440002b1fd6ccc3627096d605d559a
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-08 15:58:04 +0800

    Create __manifest__.py

---
+++ sale_orderline_duplicate/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Order Line Duplicate",
+    "version": "12.0.0.1",
+    "summary": "Add widget to duplicate order line.",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://www.safyric.com",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_line.xml",
+    ],
+    "installable": True,
+}
