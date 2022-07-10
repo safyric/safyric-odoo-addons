a2a3d4c475cc6e501270a53837125ee872109b2c
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-10 18:28:14 +0800

    Update sale_order_line.py

--- sale_order_line_vendor/models/sale_order_line.py
+++ sale_order_line_vendor/models/sale_order_line.py
@@ -1,8 +1,37 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import models, fields, api
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    
-    product_vendor = fields.Many2one('product.supplierinfo', string='Vendor', default=False)
+
+    vendor_id = fields.Many2one('product.supplierinfo', 'Vendor')
+    is_route = fields.Boolean('Is Route', defualt=False)
+    product_tmpl_id = fields.Many2one(related='product_id.product_tmpl_id',
+                                      string='Product Template',
+                                      store=True)
+
+    @api.multi
+    @api.onchange('route_id')
+    def _onchange_route_id(self):
+        if self.route_id:
+            self.is_route = True
+
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _make_po_select_supplier(self, values, suppliers):
+        """ Method intended to be overridden by customized
+        modules to implement any logic in the
+            selection of supplier.
+        """
+        if values.get('route_ids'):
+            so = self.env['sale.order.line'].browse(values.get('sale_line_id'))
+            supplier = so.vendor_id or suppliers[0]
+
+            return supplier
+        else:
+            return super(ProcurementRule, self)\
+                ._make_po_select_supplier(values, suppliers)
