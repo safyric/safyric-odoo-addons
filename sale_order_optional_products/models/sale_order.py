2ecabd83810e03a66a0b06881981624c1a0c0c50
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-19 13:04:34 +0800

    Update sale_order.py

--- sale_order_optional_products/models/sale_order.py
+++ sale_order_optional_products/models/sale_order.py
@@ -2,3 +2,7 @@ from odoo import api, fields, models
 
 class SaleOrderOption(models.Model):
     _inherit = "sale.order.option"
+
+    customer_lead = fields.Float(
+        'Delivery Lead Time', required=True, default=0.0,
+        help="Number of days between the order confirmation and the shipping of the products to the customer")
