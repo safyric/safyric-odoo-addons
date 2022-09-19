4ef72c8aa3902f4303e38d8f4ebe79150a4784d6
jchanme <39323414+jchanme@users.noreply.github.com>
2022-09-19 16:30:03 +0800

    Create __manifest__.py

---
+++ partner_contact_translatable/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Partner Contact Translatable",
+    "version": "12.0.1.0.2",
+    "depends": ['base',
+        'hr',
+                ],
+    'author': 'Safyric Co., Ltd.',
+    "category": "Localization",
+    'license': 'AGPL-3',
+    'installable': True,
+}
