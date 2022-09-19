7e1f69adf0e3b78ff0694f249ac42f8e7db3a60f
jchanme <39323414+jchanme@users.noreply.github.com>
2022-09-19 16:09:52 +0800

    Create __manifest__.py

---
+++ base_country_translatable/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Translate Country States",
+    "version": "12.0.1",
+    "depends": ['base'],
+    "website": 'https://www.github.com/safyric',
+    "summary": "Translate Country States",
+    'author': 'www.safyric.com',
+    "category": "Localisation",
+    'license': 'AGPL-3',
+    'installable': True,
+}
