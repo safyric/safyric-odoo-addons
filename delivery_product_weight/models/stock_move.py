ad7a3b29963a29861cececd0718f4461eb2c5885
jchanme <39323414+jchanme@users.noreply.github.com>
2022-10-13 09:57:24 +0800

    Update stock_move.py

--- delivery_product_weight/models/stock_move.py
+++ delivery_product_weight/models/stock_move.py
@@ -6,7 +6,7 @@ from odoo.addons import decimal_precision as dp
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    product_weight = fields.Float('Product Weight', default=lambda self: self.product_id.weight, digits=dp.get_precision('Stock Weight'))
+    product_weight = fields.Float('Product Weight', digits=dp.get_precision('Stock Weight'))
 
     @api.depends('product_id', 'product_uom_qty', 'product_uom')
     def _cal_move_weight(self):
@@ -20,4 +20,4 @@ class StockMove(models.Model):
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    product_weight = fields.Float('Product Weight', digits=dp.get_precision('Stock Weight'), related='move_id.product_weight')
+    product_weight = fields.Float('Product Weight', default=lambda self: self.product_id.weight, digits=dp.get_precision('Stock Weight'), related='move_id.product_weight')
