c3592ac3caa6dfe595e9dfe6a3e2c7eed7678643
jchanme <39323414+jchanme@users.noreply.github.com>
2022-10-30 08:46:29 +0800

    Create __manifest__.py

---
+++ partner_intermediary_bank/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Partner Intermediary Bank',
+    'summary': "This module allows to add intermediary bank.",
+    'version': '12.0.1.0.0',
+    'development_status': "Production/Stable",
+    'license': 'AGPL-3',
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_bank.xml',
+    ],
+}
