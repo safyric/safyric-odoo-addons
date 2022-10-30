36a45c2ae7fb0904afa1ac689634041df4626eac
jchanme <39323414+jchanme@users.noreply.github.com>
2022-10-30 08:35:39 +0800

    Create __manifest__.py

---
+++ partner_bank_view/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Partner Bank View',
+    'summary': "This module modifies views and adds reports menu.",
+    'version': '12.0.1.0.0',
+    'development_status': "Production/Stable",
+    'license': 'AGPL-3',
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/report_partner_bank.xml',
+    ],
+}
