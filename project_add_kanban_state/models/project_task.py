b464d2ec97d80c3e2cc57bfc13f4874a42d0f3d4
jchanme <39323414+jchanme@users.noreply.github.com>
2023-02-23 19:37:07 +0800

    Update project_task.py

--- project_add_kanban_state/models/project_task.py
+++ project_add_kanban_state/models/project_task.py
@@ -5,3 +5,15 @@ class ProjectTask(models.Model):
     """Added state in the Project Task."""
 
     _inherit = 'project.task'
+
+    legend_inprogress = fields.Char(related='stage_id.legend_inprogress', string='Kanban Ongoing Explanation', readonly=True, related_sudo=False)
+
+    kanban_state = fields.Selection(selection_add=[('inprogress', 'Yellow')])
+    
+    @api.depends('stage_id', 'kanban_state')
+    def _compute_kanban_state_label(self):
+        res = super(ProjectTask, self)._compute_kanban_state_label()
+        for task in self:
+            if task.kanban_state == 'inprogress':
+                task.kanban_state_label = task.legend_inprogress
+        return res
