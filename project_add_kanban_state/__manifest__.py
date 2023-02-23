90d6574cf15babbb436124f07792bccc779cac3b
jchanme <39323414+jchanme@users.noreply.github.com>
2023-02-23 19:54:25 +0800

    Update __manifest__.py

--- project_add_kanban_state/__manifest__.py
+++ project_add_kanban_state/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Add Kanban State',
+    'name': 'Project Add Kanban State',
     'version': '12.0.1.0.0',
     'category': 'Project Management',
     'summary': 'Add more states to project task stages',
     'author': "Safyric Co., Ltd.",
-    'website': 'https://github.com/safyric/safyric-odoo-addons,
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
     'license': 'AGPL-3',
     'installable': True,
     'depends': [
