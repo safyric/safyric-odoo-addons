7240fda689e12601d1b5a150a5e51684619800d2
jchanme <39323414+jchanme@users.noreply.github.com>
2023-02-24 15:01:38 +0800

    Update project_task_type.py

--- project_add_kanban_state/models/project_task_type.py
+++ project_add_kanban_state/models/project_task_type.py
@@ -10,5 +10,5 @@ class ProjectTaskType(models.Model):
         help='Override the default value displayed for the normal state for kanban selection, when the task or issue is in that stage.')
     
     legend_inprogress = fields.Char(
-        'Yellow Kanban Label', default=lambda s: _('In Progress'), translate=True, required=True,
+        'Grey Kanban Label', default=lambda s: _('In Progress'), translate=True, required=True,
         help='Override the default value displayed for the in progress state for kanban selection, when the task or issue is in that stage.')
