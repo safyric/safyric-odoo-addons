5a38b3504b4bedfb31ffd121b2f51c27e6c0fb3e
jchanme <39323414+jchanme@users.noreply.github.com>
2023-02-24 17:23:06 +0800

    Create __manifest__.py

---
+++ web_widget_state_selection/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'State Selection Widget',
+    'summary': 'This module adds support for state selection widget',
+    'category': 'Web',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/assets.xml',
+    ],
+    'installable': True,
+}
