1333fda89c20de2e7243e5f27b755a5fb6f86464
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-01 14:07:29 +0800

    Create __manifest__.py

---
+++ base_bank_from_china/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Bank from IBAN",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_bank_view.xml",
+    ],
+    'installable': True,
+}
