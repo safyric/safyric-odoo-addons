03c04f03b8c6683202faee8dbd541cf8cb6f7e44
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-01 14:13:35 +0800

    Update res_bank.py

--- base_bank_from_china/models/res_bank.py
+++ base_bank_from_china/models/res_bank.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class ResBank(models.Model):
     _inherit = 'res.bank'
 
-    cnaps_code = fields.Char()
+    cnaps_code = fields.Char(string="CNAPS Code", help="银行联行号是一个地区银行的唯一识别标志。")
