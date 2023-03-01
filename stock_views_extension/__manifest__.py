47b6124b6fdda46b75488902cb7731cb5d852d17
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-01 18:59:55 +0800

    Update __manifest__.py

--- stock_views_extension/__manifest__.py
+++ stock_views_extension/__manifest__.py
@@ -15,6 +15,7 @@
         "views/stock_report_layout.xml",
         "views/report_deliveryslip.xml",
         "views/report_picking.xml",
+        "views/stock_move_view.xml",
         "views/stock_picking_view.xml",
         "views/stock_delivery_report.xml",
     ],
