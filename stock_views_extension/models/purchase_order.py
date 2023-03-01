cc8e76d0b9aad737254bc9babee9e36210020ae8
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-01 20:27:38 +0800

    Update purchase_order.py

--- stock_views_extension/models/purchase_order.py
+++ stock_views_extension/models/purchase_order.py
@@ -1,1 +1,11 @@
+from odoo import fields, models
 
+class PurchaseOrdreLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def name_get(self):
+      result = []
+      for rec in self:
+          result.append((rec.id, product_id))
+
+      return result
