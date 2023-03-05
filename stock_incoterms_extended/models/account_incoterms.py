633b3881962e1cc133eabcef580561e6c3d62c76
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-05 12:44:08 +0800

    Update account_incoterms.py

--- stock_incoterms_extended/models/account_incoterms.py
+++ stock_incoterms_extended/models/account_incoterms.py
@@ -3,11 +3,13 @@ from odoo import fields, models
 class AccountIncoterms(models.Model):
     _inherit = 'account.incoterms'
 
+    
+    edition = fields.Char(string="Incoterms Edition")
+    
     def name_get(self):
         result = []
         for rec in self:
-            result.append((rec.id, '%s - %s' % (rec.code,rec.name)))
+            result.append((rec.id, '%s - %s' % (rec.code,rec.edition)))
             
         return result
-    
-    edition = fields.Char(string="Incoterms Edition")
+
