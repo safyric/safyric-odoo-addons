d55b7f76ee080ed7d2750d3023c5327a107d5656
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-05 12:53:38 +0800

    Update __manifest__.py

--- stock_incoterms_extended/__manifest__.py
+++ stock_incoterms_extended/__manifest__.py
@@ -14,7 +14,7 @@
         'views/sale_order_view.xml',
         'views/stock_delivery_view.xml',
         'views/report_sale_order.xml',
-        'views/account_incoterms_views.xml'，
+        'views/account_incoterms_views.xml'
     ],
     "depends": [
         "stock",
