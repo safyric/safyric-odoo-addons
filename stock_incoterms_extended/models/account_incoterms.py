9aae4b890a2f13c5f136f805b31e639b34b8b7f3
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-05 12:43:13 +0800

    Update account_incoterms.py

--- stock_incoterms_extended/models/account_incoterms.py
+++ stock_incoterms_extended/models/account_incoterms.py
@@ -4,8 +4,10 @@ class AccountIncoterms(models.Model):
     _inherit = 'account.incoterms'
 
     def name_get(self):
-      result = []
-      for rec in self:
-          result.append((rec.id, '%s - %s' % (rec.code,rec.name)))
-
-      return result
+        result = []
+        for rec in self:
+            result.append((rec.id, '%s - %s' % (rec.code,rec.name)))
+            
+        return result
+    
+    edition = fields.Char(string="Incoterms Edition")
