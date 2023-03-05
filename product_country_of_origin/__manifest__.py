725efaf875729e41b42a047cba5966caf884852e
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-05 13:47:42 +0800

    Create __manifest__.py

---
+++ product_country_of_origin/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Product Country of Origin',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Add country of origin to product.',
+    'depends': [
+        'product',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/product_views.xml',
+    ],
+    'installable': True,
+}
