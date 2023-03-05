c00c1c08c46df0153c94334ece654ed3d2953ce4
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-05 13:54:29 +0800

    Update product_template.py

--- product_country_of_origin/models/product_template.py
+++ product_country_of_origin/models/product_template.py
@@ -1,3 +1,6 @@
 from odoo import models, fields
 
-class ProductTemplate(models.Model)：
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    country_of_origin_id = fields.Many2one('res.country', string='Country of Origin')
