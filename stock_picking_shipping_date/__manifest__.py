4af5f5fedd5c7036f0babe9605c33ff0f0232e0b
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-08 20:31:02 +0800

    Create __manifest__.py

---
+++ stock_picking_shipping_date/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Picking Shipping Date',
+    'summary': 'Add shipping date to stock picking',
+    'version': '12.0.1.0.0',
+    'category': 'Warehouse Management',
+    'author': "Safyric Co., Ltd.",
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/report_deliveryslip.xml',
+        'views/stock_view.xml',
+        'views/account_invoice_view.xml'
+    ],
+    'installable': True,
+    'license': "AGPL-3",
+}
