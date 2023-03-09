9450e6cb0f7bea6f94c89ad6ba3e531092edaf87
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-09 22:32:55 +0800

    Create sale_order_line.py

---
+++ sale_order_line_barcode/models/sale_order_line.py
@@ -0,0 +1,6 @@
+from odoo import api, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    
+    barcode = fields.Char(related='product_id.barcode', string="Barcode", store=True).
