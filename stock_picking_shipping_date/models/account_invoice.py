2ee739ce5029989c633edf46c1e932deccca1d42
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-09 10:30:58 +0800

    Update account_invoice.py

--- stock_picking_shipping_date/models/account_invoice.py
+++ stock_picking_shipping_date/models/account_invoice.py
@@ -1,6 +1,13 @@
-from odoo import api, models, fields
+from odoo import api, models, fields, _
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     
     picking_id = fields.Many2one('stock.picking')
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        for rec in self:
+            return {'domain': {'picking_id': [('partner_id', '=', rec.partner_id.id)]}}
+        return super(AccountInvoice, self)._onchange_partner_id()
+    
