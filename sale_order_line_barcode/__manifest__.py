64a1e40786b07bfe12076a5d7cfd4f7a04392be2
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-09 22:30:23 +0800

    Create __manifest__.py

---
+++ sale_order_line_barcode/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Order Line Barcode",
+    "summary": "Add barcode to sale order line",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "category": "Sales",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "license": "AGPL-3",
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    "depends": [
+        "sale",
+    ],
+    "installable": True,
+}
