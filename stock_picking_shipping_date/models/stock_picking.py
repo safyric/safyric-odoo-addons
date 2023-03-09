a08b05c578d91dcc0657550da97f002b781ceed4
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-09 15:48:22 +0800

    Update stock_picking.py

--- stock_picking_shipping_date/models/stock_picking.py
+++ stock_picking_shipping_date/models/stock_picking.py
@@ -6,3 +6,21 @@ class StockPicking(models.Model):
     
     
     shipping_date = fields.Date(string='Shipping Date')
+
+    def get_invoice(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Invoices',
+            'view_mode': 'tree',
+            'res_model': 'account.invoice',
+            'domain': [('picking_id', '=', self.id)],
+            'context': "{'create': False}"
+        }
+    
+    invoice_count = fields.Integer(compute='compute_count')
+    
+    def compute_count(self):
+    for record in self:
+        record.invoice_count = self.env['account.invoice'].search_count(
+            [('picking_id', '=', self.id)])
