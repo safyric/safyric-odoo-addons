a57d1b5078b39abc42cf129bae445b977ac960c9
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-10 15:00:58 +0800

    Update purchase_order.py

--- purchase_order_propagate_price/models/purchase_order.py
+++ purchase_order_propagate_price/models/purchase_order.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api
+from odoo import api, models
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
