02547fbf9ca8a6f51f51c35d453f0ca5ccdb7b84
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-10 14:34:44 +0800

    Create __manifest__.py

---
+++ purchase_order_propogate_price/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Purchase Order Propogate Price",
+    'summary': 'Propogate purchase price to Sales Order',
+    "author": "Safyric Co., Ltd.",
+    "version": "12.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
