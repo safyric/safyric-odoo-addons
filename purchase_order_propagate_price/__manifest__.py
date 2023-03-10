3a30d97c8142f2f9be7afa4f88ef03996a2cd979
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-10 14:58:05 +0800

    Update __manifest__.py

--- purchase_order_propagate_price/__manifest__.py
+++ purchase_order_propagate_price/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    "name": "Purchase Order Propogate Price",
-    'summary': 'Propogate purchase price to Sales Order',
+    "name": "Purchase Order Propagate Price",
+    'summary': 'Propagate purchase price to Sales Order',
     "author": "Safyric Co., Ltd.",
     "version": "12.0.1.0.0",
     "category": "Purchase Management",
