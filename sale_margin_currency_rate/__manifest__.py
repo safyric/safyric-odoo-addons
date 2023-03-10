4f5747133a011295eeacb04a413c83aa0e932196
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-11 07:46:09 +0800

    Create __manifest__.py

---
+++ sale_margin_currency_rate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sale Margin Currency Rate",
+    "version": "12.0.0.1",
+    "summary": "Use the purchase price in company currency and calculate margin.",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_margin",
+        "sale",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+}
