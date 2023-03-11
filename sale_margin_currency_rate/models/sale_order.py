b7e4e27142c06238cb70474614fa1af120a2b1ac
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-11 15:19:53 +0800

    Update sale_order.py

--- sale_margin_currency_rate/models/sale_order.py
+++ sale_margin_currency_rate/models/sale_order.py
@@ -1,14 +1,15 @@
 from odoo import api, fields, models, _
 
 
-class SaleOrderLineCurrency(models.Model):
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
     
     @api.depends('product_id', 'purchase_price', 'product_uom_qty', 'price_unit', 'price_subtotal')
     def _product_margin(self):
+        res = super(SaleOrderLine, self)._product_margin()
         for line in self:
             currency = line.order_id.pricelist_id.currency_id
             currency_rate = line.order_id.currency_rate
             price = line.purchase_price * currency_rate
             line.margin = currency.round(line.price_subtotal - (price * line.product_uom_qty))
-        return super(SaleOrderLine, self)._product_margin()
+        return res
