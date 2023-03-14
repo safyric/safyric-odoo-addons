b1d30b5069c6ac5ae408538c4f5025b0649dc8f5
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-14 16:09:09 +0800

    Update stock_move.py

--- delivery_product_weight/models/stock_move.py
+++ delivery_product_weight/models/stock_move.py
@@ -21,7 +21,7 @@ class StockMove(models.Model):
     @api.onchange('product_id')
     def onchange_product_id(self):
         res = super(StockMove, self).onchange_product_id()
-        if self.product_weight != self.product_id.weight:
+        if self.product_weight > 0 and self.product_weight != self.product_id.weight:
             self.product_weight = self.product_id.weight
         return res
     
@@ -33,7 +33,7 @@ class StockMoveLine(models.Model):
 
     @api.onchange('product_id')
     def onchange_product_id(self):
-        if self.product_weight != self.product_id.weight:
+        if self.product_weight > 0 and self.product_weight != self.product_id.weight:
             self.product_weight = lambda self: self.product_id.weight
         
         return super(StockMoveLine, self).onchange_product_id()
