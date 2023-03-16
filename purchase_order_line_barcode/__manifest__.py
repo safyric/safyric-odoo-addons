0d38802e16349505404b8743acb82b8a0e93444f
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-16 12:06:32 +0800

    Create __manifest__.py

---
+++ purchase_order_line_barcode/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Purchase Order Line Barcode",
+    "summary": "Add barcode to purchase order line",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "category": "Purchase",
+    "website": "https://github.com/safyric/safyric-odoo-addons",
+    "license": "AGPL-3",
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    "depends": [
+        "purchase",
+    ],
+    "installable": True,
+}
