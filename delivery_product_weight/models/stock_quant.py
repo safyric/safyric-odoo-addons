61c56378143c8a5a3972d68bafaa6f747cf2a101
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-17 15:41:42 +0800

    Update stock_quant.py

--- delivery_product_weight/models/stock_quant.py
+++ delivery_product_weight/models/stock_quant.py
@@ -7,13 +7,14 @@ class StockQuantPackage(models.Model):
     _inherit = "stock.quant.package"
     
     shipping_weight = fields.Float(string='Shipping Weight', compute='_compute_weight', help="Weight used to compute the price of the delivery (if applicable).")
+    package_weight = fields.Float(string='Package Weight', help="Weight of packages used to compute the price of the delivery (if applicable).")
 
     @api.one
     @api.depends('quant_ids', 'packaging_id')
     def _compute_weight(self):
         res = super(StockQuantPackage, self)._compute_weight()
         weight = 0.0
-        package_weight = self.packaging_id.weight
+        package_weight = 0.0
         if self.env.context.get('picking_id'):
             current_picking_move_line_ids = self.env['stock.move.line'].search([('result_package_id', '=', self.id), ('picking_id', '=', self.env.context['picking_id'])])
             for ml in current_picking_move_line_ids:
@@ -21,6 +22,9 @@ class StockQuantPackage(models.Model):
                     weight += ml.product_uom_id._compute_quantity(ml.qty_done,ml.product_id.uom_id) * ml.product_weight
                 else:
                     weight += ml.product_uom_id._compute_quantity(ml.qty_done,ml.product_id.uom_id) * ml.product_id.weight
+
+        if self.packaging_id and self.packaging_id.weight > 0:
+            package_weight = self.packaging_id.weight
         
         self.weight = weight
         self.shipping_weight = weight + package_weight
