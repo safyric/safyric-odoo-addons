f130d1047a79ec2958e4fedf0656e1f4c6845764
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-20 19:36:49 +0800

    Create purchase_order.py

---
+++ purchase_order_line_description_safyric/models/purchase_order.py
@@ -0,0 +1,38 @@
+from odoo import api, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        res = super(PurchaseOrderLine, self).onchange_product_id()
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang
+        )
+
+        self.name = self.get_purchase_order_line_multiline_description_purchase(product)
+
+        return res
+
+    def get_purchase_order_line_multiline_description_purchase(self, product):
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang
+        )
+
+        if product and product.description_purchase:
+            return product.display_name + "\n" + self._get_purchase_order_line_multiline_description_variants().strip(', ') + product.description_purchase
+        elif product:
+            return product.display_name + "\n" + self._get_purchase_order_line_multiline_description_variants().strip(', ')
+
+
+    def _get_purchase_order_line_multiline_description_variants(self):
+        name = ""
+
+        # attribute_value_with_variants
+        for pav in self.product_id.attribute_value_ids.with_context(lang=self.order_id.partner_id.lang or self.env.lang):
+            attribute_id = pav.attribute_id.with_context(lang=self.order_id.partner_id.lang or self.env.lang)
+            name += attribute_id.name + ': ' + pav.name + "\n"
+
+        return name
