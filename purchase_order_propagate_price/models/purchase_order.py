ac72ccec961333c319efdc47c30342672a72fc97
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-20 13:30:02 +0800

    Update purchase_order.py

--- purchase_order_propagate_price/models/purchase_order.py
+++ purchase_order_propagate_price/models/purchase_order.py
@@ -9,7 +9,7 @@ class PurchaseOrderLine(models.Model):
     def write(self, values):
         for line in self:
             if line.sale_line_id:
-                price_untaxed = self.price_unit - self.price_tax
+                price_untaxed = line.price_unit - line.price_tax
                 line.sale_line_id.purchase_price = price_untaxed
                 line.product_id.standard_price = price_untaxed
         return super(PurchaseOrderLine, self).write(values)
