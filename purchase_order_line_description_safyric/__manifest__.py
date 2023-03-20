3484bc132babab18f67040e067096b3656f1741e
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-20 19:30:18 +0800

    Create __manifest__.py

---
+++ purchase_order_line_description_safyric/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase order line description",
+    "version": "12.0.1.0.0",
+    "category": "Purchase",
+    "author": "Safyric Co., Ltd.",
+    "website": "https://github.com/safyric/safyric-odoo-addons/",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+        "product"
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
