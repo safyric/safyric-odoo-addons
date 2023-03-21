fcffaf1785066802609be3bdd83cbc8d77142772
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-21 14:17:06 +0800

    Create __manifest__.py

---
+++ purchase_terms_condition/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Terms and Conditionis for Purchase Order",
+    "version": "12.0.1.1",
+    "depends": ['purchase',
+                'purchase_default_terms_conditions'
+                ],
+    'author': 'Safyric Co., Ltd.',
+    'license': 'AGPL-3',
+    'installable': True,
+}
