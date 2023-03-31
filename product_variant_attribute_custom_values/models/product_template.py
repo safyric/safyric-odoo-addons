3e47eec6e78da6806886c2085305618c41bd5f51
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 22:23:54 +0800

    Update product_template.py

--- product_variant_attribute_custom_values/models/product_template.py
+++ product_variant_attribute_custom_values/models/product_template.py
@@ -3,13 +3,13 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    variant_attribute_custom_values = fields.One2many('product.attribute.custom.value', 
+    variant_attribute_custom_values = fields.One2many('product.variant.attribute.custom.value', 
                                                       compute='_compute_variant_attribute_custom_values', 
                                                       string='Variant Attribute Custom Values')
     
     @api.depends('product_variant_ids.attribute_value_ids')
     def _compute_variant_attribute_custom_values(self):
         for template in self:
-            template.variant_attribute_custom_values = self.env['product.attribute.custom.value']
+            template.variant_attribute_custom_values = self.env['product.variant.attribute.custom.value']
             for variant in template.product_variant_ids:
                 template.variant_attribute_custom_values |= variant.attribute_custom_value_ids
