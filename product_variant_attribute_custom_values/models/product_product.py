776c61f7c13d6bd5388a159823a2cfd130ef103c
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 23:19:12 +0800

    Delete product_product.py

--- product_variant_attribute_custom_values/models/product_product.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-    
-    attribute_custom_value_ids = fields.One2many('product.variant.attribute.custom.value', 'product_id', string='Attribute Custom Values')
