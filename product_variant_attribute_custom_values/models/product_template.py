d7677be238cf83efdc2557ac1e391941f34573b1
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 23:05:50 +0800

    Update product_template.py

--- product_variant_attribute_custom_values/models/product_template.py
+++ product_variant_attribute_custom_values/models/product_template.py
@@ -12,36 +12,3 @@ class ProductTemplate(models.Model):
             for variant in template.product_variant_ids:
                 template.variant_attribute_custom_values |= variant.attribute_custom_value_ids
 
-    
-    @api.model
-    def create(self, vals):
-        res = super(ProductTemplate, self).create(vals)
-        if vals.get('attribute_line_ids'):
-            for attribute_line in vals['attribute_line_ids']:
-                for value in attribute_line[2]['value_ids']:
-                    if value[2].get('custom_value'):
-                        self.env['product.variant.attribute.custom.value'].create({
-                            'product_id': res.product_variant_id.id,
-                            'name': value[2]['custom_value'],
-                            'attribute_value_id': value[2]['id'],
-                        })
-        return res
-    
-    
-    def write(self, vals):
-        res = super(ProductTemplate, self).write(vals)
-        if vals.get('attribute_line_ids'):
-            for template in self:
-                for attribute_line in vals['attribute_line_ids']:
-                    for value in attribute_line[2]['value_ids']:
-                        if value[2].get('custom_value'):
-                            existing_custom_value = template.product_variant_id.attribute_custom_value_ids.filtered(lambda r: r.attribute_value_id == value[2]['id'])
-                            if existing_custom_value:
-                                existing_custom_value.write({'name': value[2]['custom_value']})
-                            else:
-                                self.env['product.variant.attribute.custom.value'].create({
-                                    'product_id': template.product_variant_id.id,
-                                    'name': value[2]['custom_value'],
-                                    'attribute_value_id': value[2]['id'],
-                                })
-        return res
