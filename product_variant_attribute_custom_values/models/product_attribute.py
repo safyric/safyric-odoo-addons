f3964b5e3703b2451465f09ff02ce1e0077a6d87
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 23:17:44 +0800

    Update product_attribute.py

--- product_variant_attribute_custom_values/models/product_attribute.py
+++ product_variant_attribute_custom_values/models/product_attribute.py
@@ -1,14 +1,9 @@
 from odoo import api, fields, models
 
-class ProductAttributeValue(models.Model):
+class ProductVariantAttributeCustomValue(models.Model):
     _name = 'product.variant.attribute.custom.value'
     _description = 'Product Variant Attribute Custom Value'
     
     name = fields.Char(string='Value')
     attribute_value_id = fields.Many2one('product.attribute.value', string='Attribute Value')
     product_id = fields.Many2one('product.product', string='Product Variant')
-
-class ProductAttributeValue(models.Model):
-    _inherit = 'product.attribute.value'
-    
-    variant_custom_value_ids = fields.One2many('product.variant.attribute.custom.value', 'attribute_value_id', string='Variant Attribute Custom Values')
