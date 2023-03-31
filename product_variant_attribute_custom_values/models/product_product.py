7be3d213a186aba103548b88a139ff8b5d43f4cb
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 22:08:57 +0800

    Update product_product.py

--- product_variant_attribute_custom_values/models/product_product.py
+++ product_variant_attribute_custom_values/models/product_product.py
@@ -5,3 +5,15 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
     
     attribute_custom_value_ids = fields.One2many('product.attribute.custom.value', 'product_id', string='Attribute Custom Values')
+
+    
+    @api.model
+    def create(self, vals):
+        res = super(ProductProduct, self).create(vals)
+        if vals.get('attribute_custom_value_ids'):
+            self.env['product.attribute.custom.value'].create({
+                'product_id': res.id,
+                'name': vals['name'],
+                'attribute_id': vals['attribute_custom_value_ids']['attribute_id'],
+            })
+        return res
