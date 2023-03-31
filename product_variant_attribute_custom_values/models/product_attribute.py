73793912ee96bf64bb0963a03a620ac29db7f491
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 22:39:11 +0800

    Update product_attribute.py

--- product_variant_attribute_custom_values/models/product_attribute.py
+++ product_variant_attribute_custom_values/models/product_attribute.py
@@ -4,5 +4,5 @@ class ProductAttributeValue(models.Model):
     _name = 'product.variant.attribute.custom.value'
     
     name = fields.Char(string='Value')
-    attribute_id = fields.Many2one('product.attribute', string='Attribute')
+    attribute_value_id = fields.Many2one('product.attribute.value', string='Attribute')
     product_id = fields.Many2one('product.product', string='Product Variant')
