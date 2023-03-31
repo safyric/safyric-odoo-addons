4855e27d6b319135c73c5b2f274f264841a8fd7c
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 23:14:31 +0800

    Update product_product.py

--- product_variant_attribute_custom_values/models/product_product.py
+++ product_variant_attribute_custom_values/models/product_product.py
@@ -5,5 +5,3 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
     
     attribute_custom_value_ids = fields.One2many('product.variant.attribute.custom.value', 'product_id', string='Attribute Custom Values')
-
-
