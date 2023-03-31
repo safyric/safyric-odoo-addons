7d1343e1da626e3b26d1c3ebd99136328ac02796
jchanme <39323414+jchanme@users.noreply.github.com>
2023-03-31 22:01:28 +0800

    Create __manifest__.py

---
+++ product_variant_attribute_custom_values/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Product Variant with Attribute Custom Values',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Link attribute custom values to product variant level.',
+    'depends': [
+        'product',
+    ],
+    'author': 'Safyric Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'data': [
+        'views/product_template.xml',
+        'views/product.xml'
+    ],
+    'installable': True,
+}
