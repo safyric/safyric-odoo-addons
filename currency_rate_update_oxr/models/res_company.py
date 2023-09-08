82c13f65cafe6758d819101a4edb180af20b9e64
James Chan <james.chan@live.com>
2023-09-08 12:52:13 +0800

    Initial commit

---
+++ currency_rate_update_oxr/models/res_company.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    openexchangerates_app_id = fields.Char(
+        string='OpenExchangeRates.org App ID',
+    )
+
+    openexchangerates_is_free = fields.Boolean(
+        string='OpenExchangeRates.org Free Plan',
+    )
