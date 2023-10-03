e650dabf5ca892daba58e8e9735eb9d4ef95b15a
James Chan <james.chan@live.com>
2023-10-03 14:11:17 +0800

    Add method to change filename to lower cases

--- dms_digital_signature/models/dms_file.py
+++ dms_digital_signature/models/dms_file.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import os
+import re
 from odoo import _, api, fields, models
 
 
@@ -10,3 +12,13 @@ class DmsFile(models.Model):
 
     signed_doc = fields.Binary(attachment=False, string='Signed Document', realonly=True)
     signed_doc_name = fields.Char('Filename')
+
+
+    @api.onchange("content", "signed_doc")
+    def onchange_binaryfields(self):
+        if self.name:
+            content_name = re.sub(' +', ' ', str(self.name).replace('-', '')).replace(' ', '_').lower()
+            self.name = content_name
+        if self.signed_doc_name:
+            signed_doc_name = re.sub(' +', ' ', str(self.signed_doc_name).replace('-', '')).replace(' ', '_').lower()
+            self.signed_doc_name = signed_doc_name
