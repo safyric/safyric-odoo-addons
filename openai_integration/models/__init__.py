9902a1d22f0ea6d0f3d164ef8cc1d308bfab0dfb
James Chan <james.chan@live.com>
2023-10-31 22:07:07 +0800

    Changed directory name

--- openai_integration/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020-Present InTechual Solutions. (<https://intechualsolutions.com/>)
-
-from . import res_users
-from . import res_partners
-from . import mail_thread
-from . import mail_ai_bot
-from . import mail_channel
-from . import res_config_settings
