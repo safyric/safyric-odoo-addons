1933122d7053fdc91364c26dbdea14cda65f1ce5
James Chan <james.chan@live.com>
2023-11-05 22:28:22 +0800

    Change to different name

--- purchase_terms_condition/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ResCompany(models.Model):
-
-    _inherit = 'res.company'
-
-    purchase_note = fields.Html(
-        string='Purchase Default Terms and Conditions',
-        translate=True)
