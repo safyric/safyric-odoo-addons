d2f2c186204e063732fd832cca7e5042797c4d2d
James Chan <james.chan@live.com>
2023-11-05 19:54:25 +0800

    Add option to use 'signature' widget in the wizard

--- sale_order_digitized_signature/wizards/digital_signature_wizard.py
+++ sale_order_digitized_signature/wizards/digital_signature_wizard.py
@@ -6,7 +6,9 @@ class SaleOrderDigitalSignature(models.TransientModel):
     _name = "sale.order.digital.signature"
     _description = "Sale order Digital Signature"
 
+    signature_type = fields.Selection([('select', 'Select/Upload'), ('draw', 'Draw')], default='select', string="Signature Type")
     signature_id = fields.Many2one('res.users.signatures', string='Signature')
+    signature_draw = fields.Binary(string="Signature", attachment=False)
     signature = fields.Binary(string='Signature', attachment=False)
     signature_keyword = fields.Char('Signature Area', help="Keyword to identify the signature area to attach digital signature")
     signature_width = fields.Integer('Signature Width', help="Width of the signature area")
@@ -17,6 +19,13 @@ class SaleOrderDigitalSignature(models.TransientModel):
     def _onchange_signature_id(self):
         if self.signature_id:
             self.signature = self.signature_id.digital_signature
+        else:
+            self.signature = False
+
+    @api.onchange('signature_draw')
+    def _onchange_signature_draw(self):
+        if self.signature_draw:
+            self.signature = self.signature_draw
         else:
             self.signature = False
 
