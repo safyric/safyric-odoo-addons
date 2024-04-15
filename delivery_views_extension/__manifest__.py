f7fe01e2fa6be47292def533f41da7bc4460eaf1
James Chan <james.chan@live.com>
2024-04-15 14:16:05 +0800

    Initial commit

---
+++ delivery_views_extension/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Delivery Views Extension',
+    'version': '12.0.0.0.1',
+    'summary': "Extend delivery views",
+    'author': 'Safyric Valve Co., Ltd.',
+    'website': 'https://github.com/safyric/safyric-odoo-addons',
+    'license': 'AGPL-3',
+    'depends': [
+	'base',
+        'delivery',
+    ],
+    'data': [
+	'views/delivery_view.xml',
+    ],
+    'installable': True
+}
